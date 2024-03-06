f5568e0aec4172b65d6d94e6318a194b65e761dc
Necnion8 <necnionch@gmail.com>
2024-03-07 00:04:23 +0900

    Add: easydev にサンプルプラグインを追加

--- dncore/easydev.py
+++ dncore/easydev.py
@@ -11,3 +11,48 @@ __all__ = ["DNCoreAPI", "Plugin",
            "onevent", "oncommand", "CommandContext", "events", "Embed",
            "FileConfigValues", "ConfigValues",
            ]
+
+#
+#  Example Plugin
+#
+from logging import getLogger
+from dncore.discord.events import ReadyEvent
+
+log = getLogger(__name__)
+
+
+class ExamplePlugin(Plugin):
+    def __init__(self):
+        # set values
+        pass
+
+    async def on_enable(self):
+        # init plugin
+        pass
+
+    async def on_disable(self):
+        # cleanup plugin
+        pass
+
+    @onevent(monitor=True)
+    async def on_ready(self, _: ReadyEvent):
+        # Discord Ready
+        client = DNCoreAPI.client()
+        log.info("ログインしました: %s", client.user)
+
+    @oncommand(defaults=True)
+    async def cmd_say(self, ctx: CommandContext):
+        """
+        {command} (内容)
+        指定された内容をボットが発言してくれます
+        """
+        if not ctx.args:
+            await ctx.channel.send(content="こんにちは！")
+
+        else:
+            content = ctx.content[len(ctx.prefix + ctx.execute_name)+1:]
+            await ctx.channel.send(content=content)
+
+#
+#
+#
