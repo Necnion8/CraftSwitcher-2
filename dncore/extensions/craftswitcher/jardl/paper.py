5cfb92efee1d29c0542fce3d2a6731ef5c2810e3
Necnion8 <necnionch@gmail.com>
2024-09-11 16:15:31 +0900

    Clean: PaperMCの各プロジェクトの処理をまとめる

--- dncore/extensions/craftswitcher/jardl/paper.py
+++ dncore/extensions/craftswitcher/jardl/paper.py
@@ -3,10 +3,10 @@ import datetime
 import aiohttp
 from pydantic import BaseModel
 
-from .jardl import ServerDownloader, ServerMCVersion, ServerFile
+from .jardl import ServerDownloader, ServerMCVersion, ServerFile, SF, SV
 
 
-class PaperInfo(BaseModel):
+class ProjectInfo(BaseModel):
     project_id: str
     project_name: str
     versions: list[str]
@@ -21,47 +21,50 @@ class BuildDownloads(BaseModel):
     application: BuildDownload | None
 
 
-class PaperBuildInfo(BaseModel):
+class ProjectBuildInfo(BaseModel):
     build: int
     time: datetime.datetime
     channel: str
     downloads: BuildDownloads
 
 
-class PaperBuildsInfo(BaseModel):
+class ProjectBuildsInfo(BaseModel):
     project_id: str
     project_name: str
     version: str
-    builds: list[PaperBuildInfo]
+    builds: list[ProjectBuildInfo]
 
 
 #
 
 
-class PaperBuild(ServerFile):
-    def __init__(self, builds: "PaperBuildsInfo", info: PaperBuildInfo):
-        dl_url = f"https://api.papermc.io/v2/projects/paper" \
+class ProjectBuild(ServerFile):
+    def __init__(self, builds: "ProjectBuildsInfo", info: ProjectBuildInfo):
+        dl_url = f"https://api.papermc.io/v2/projects/{builds.project_id}" \
                  f"/versions/{builds.version}" \
                  f"/builds/{info.build}" \
                  f"/downloads/{info.downloads.application.name}"
         super().__init__(builds.version, str(info.build), dl_url, updated_datetime=info.time, )
 
 
-class PaperVersion(ServerMCVersion[PaperBuild]):
-    def __init__(self, mc_version: str):
+class ProjectVersion(ServerMCVersion[ProjectBuild]):
+    def __init__(self, project_id: str, mc_version: str):
         super().__init__(mc_version, None)
+        self.project_id = project_id
 
-    async def _list_builds(self) -> list[PaperBuild]:
-        url = f"https://api.papermc.io/v2/projects/paper/versions/{self.mc_version}/builds"
+    async def _list_builds(self) -> list[SF]:
+        url = f"https://api.papermc.io/v2/projects/{self.project_id}/versions/{self.mc_version}/builds"
         async with aiohttp.request("GET", url) as res:
             res.raise_for_status()
-            info = PaperBuildsInfo.model_validate_json(await res.json())
-            return [PaperBuild(info, build) for build in info.builds]
+            info = ProjectBuildsInfo.model_validate_json(await res.json())
+            return [ProjectBuild(info, build) for build in info.builds]
 
 
-class PaperServerDownloader(ServerDownloader[PaperVersion]):
-    async def _list_versions(self) -> list[PaperVersion]:
-        async with aiohttp.request("GET", "https://api.papermc.io/v2/projects/paper") as res:
+class PaperServerDownloader(ServerDownloader[ProjectVersion]):
+    project_id = "paper"
+
+    async def _list_versions(self) -> list[SV]:
+        async with aiohttp.request("GET", f"https://api.papermc.io/v2/projects/{self.project_id}") as res:
             res.raise_for_status()
-            info = PaperInfo.model_validate_json(await res.json())
-            return [PaperVersion(ver) for ver in info.versions]
+            info = ProjectInfo.model_validate_json(await res.json())
+            return [ProjectVersion(self.project_id, ver) for ver in info.versions]
