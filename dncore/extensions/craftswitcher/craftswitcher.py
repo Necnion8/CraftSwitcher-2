1ea9c8180b8511c1338dede40436367f2b7eae60
Necnion8 <necnionch@gmail.com>
2024-09-23 11:47:15 +0900

    Add: CORS を一時的に '*' allow に設定

--- dncore/extensions/craftswitcher/craftswitcher.py
+++ dncore/extensions/craftswitcher/craftswitcher.py
@@ -59,7 +59,13 @@ class CraftSwitcher(EventListener):
             title="CraftSwitcher",
             version=__version__,
         )
-        api.add_middleware(CORSMiddleware, allow_origins=["*"])
+        api.add_middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
 
         @api.on_event("startup")
         async def _startup():
