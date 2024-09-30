4ef83cb34f4aa98a3ced897735725bf8356d6b96
Necnion8 <necnionch@gmail.com>
2024-09-30 15:49:49 +0900

    Add: /config/app にJavaパスの設定を追加

--- dncore/extensions/craftswitcher/publicapi/model.py
+++ dncore/extensions/craftswitcher/publicapi/model.py
@@ -17,6 +17,11 @@ if TYPE_CHECKING:
 
 class SwitcherConfig(BaseModel):
     servers_location: str = Field("/", description="サーバーの保管に使うパス")
+    java_executables: list[str] = Field(description="実行可能なJavaのパスリスト。システムパスで指定してください。")
+    java_auto_detect_locations: list[str] = Field([
+        "/usr/lib/jvm",
+        "C:\\Program Files\\Java",
+    ], description="Javaを自動検出するディレクトリ。システムパスで指定してください。", )
 
 
 class Server(BaseModel):
