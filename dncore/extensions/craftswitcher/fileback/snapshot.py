e06f9f418423f5f3b9b4abd3903ac28a434e44e5
Necnion8 <necnionch@gmail.com>
2024-11-12 19:05:55 +0900

    Impl: スナップショットのツリーを作成するメソッドを追加する

--- dncore/extensions/craftswitcher/fileback/snapshot.py
+++ dncore/extensions/craftswitcher/fileback/snapshot.py
@@ -1,5 +1,7 @@
 import asyncio
 import datetime
+import shutil
+from logging import getLogger
 from pathlib import Path
 
 from .abc import SnapshotStatus, FileInfo, FileDifference
@@ -10,20 +12,21 @@ __all__ = [
     "get_file_info",
     "scan_files",
     "async_scan_files",
+    "create_files_diff",
+    "async_create_files_diff",
 ]
+log = getLogger(__name__)
 
 
 class SnapshotResult(object):
-    def __init__(self, old_dir: Path, new_dir: Path, files: list[FileDifference]):
+    def __init__(self, src_dir: Path, old_dir: Path, files: list[FileDifference]):
         """
-        old_dir: 古いファイル(前回のバックアップ)が格納されているディレクトリ
-
-        new_dir: 新しいファイル(現在)が格納されているディレクトリ
-
-        files: old_dirまたはnew_dirに含まれるファイルの一覧
+        :param src_dir: 現在のファイルが格納されているディレクトリ
+        :param old_dir: 古いファイル(前回のバックアップ)が格納されているディレクトリ
+        :param files: old_dirまたはnew_dirに含まれるファイルの一覧
         """
+        self.src_dir = src_dir
         self.old_dir = old_dir
-        self.new_dir = new_dir
         self.files = files
 
 
@@ -64,3 +67,46 @@ def scan_files(src_dir: Path) -> dict[str, FileInfo]:
 
 async def async_scan_files(src_dir: Path) -> dict[str, FileInfo]:
     return await asyncio.get_running_loop().run_in_executor(None, scan_files, src_dir)
+
+
+def create_files_diff(result: SnapshotResult, dst_dir: Path):
+    """
+    :param result: スナップショットスキャンの結果
+    :param dst_dir: 新しいスナップショットの作成先ディレクトリ
+    """
+    errors = []  # type: list[tuple[FileDifference, Exception]]
+    for file in result.files:
+        src_file_path = result.src_dir / file.path
+        old_file_path = result.old_dir / file.path
+        dst_file_path = dst_dir / file.path
+
+        if SnapshotStatus.LINK == file.status:
+            try:
+                old_file_path.symlink_to(dst_file_path)
+            except Exception as e:
+                log.warning("Failed to link file: %s TO %s", old_file_path, dst_file_path)
+                errors.append((file, e))
+
+        elif file.status in (SnapshotStatus.CREATE, SnapshotStatus.UPDATE, ):
+            try:
+                shutil.copy2(src_file_path, dst_file_path)
+            except Exception as e:
+                log.warning("Failed to copy file: %s TO %s", src_file_path, dst_file_path)
+                errors.append((file, e))
+
+        elif SnapshotStatus.DELETE == file.status:
+            pass
+
+        else:
+            errors.append((file, NotImplementedError(f"Unknown snapshot status: {file.status.name}")))
+            log.warning("Unknown snapshot status: %s", file.status)
+
+    return errors
+
+
+async def async_create_files_diff(result: SnapshotResult, dst_dir: Path):
+    """
+    :param result: スナップショットスキャンの結果
+    :param dst_dir: 新しいスナップショットの作成先ディレクトリ
+    """
+    return await asyncio.get_running_loop().run_in_executor(None, create_files_diff, result, dst_dir)
